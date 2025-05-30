6d6a1d1877f0f7eb9f1830397e3a9c5d2bb949d1
Jose Miguel Tolentino <85689172+sirmoonman@users.noreply.github.com>
2025-05-30 22:37:31 +0800

    Add files via upload

---
+++ Personal Expense Tracker/TransactionStore.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public static class TransactionStore
+{
+    public static List<ExpenseSave> Transactions { get; } = new List<ExpenseSave>();
+}
+// Tolentino Jose - 
